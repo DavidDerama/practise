04fdd24ccd41426c2e47eb2cb3f4020f14a9400d
DavidDerama <davidbrentderama@gmail.com>
2024-06-30 11:11:30 +0300

    fully responsive and finsiehd theme look

--- Restaurant App/script.js
+++ Restaurant App/script.js
@@ -9,9 +9,25 @@ const modal = document.querySelector(".modal");
 const form = document.getElementById("detail-info");
 const modalOpener = document.getElementById("complete");
 const headerClick = document.querySelector("header");
+const themeChange = document.getElementById("theme-btn");
+const localStorageTheme = localStorage.getItem("isLightMode");
 let cart = [];
+let isLightMode = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  if (localStorageTheme.includes("disabled")) {
+    document.body.style.background = "#1c1c1c";
+    localStorage.setItem("isLightMode", "disabled");
+    themeChange.classList.remove("fa-sun");
+    themeChange.classList.add("fa-moon");
+  } else if (localStorageTheme.includes("enabled")) {
+    document.body.style.background =
+      "radial-gradient(circle, #16db99, #00c3b6, #00a7c5, #0089c1, #0069a9)";
+    themeChange.classList.remove("fa-moon");
+    themeChange.classList.add("fa-sun");
+    localStorage.setItem("isLightMode", "enabled");
+  }
+
   let html = "";
   //   html += menuArray
   //     .map((product) => {
@@ -63,7 +79,7 @@ document.addEventListener("click", (e) => {
       renderCart();
     }
     notifEl.innerHTML = `  <div class="notif notif-animation">
-      <i class="fa-solid fa-check"></i><label>Item Remeoved from Order</label>
+      <i class="fa-solid fa-trash"></i><label>Item Remeoved from Order</label>
     </div>`;
   }
 });
@@ -75,7 +91,6 @@ function renderCart() {
   cart.forEach((order, index) => {
     const priceFinder = menuArray.find((menu) => menu.name.includes(order));
     totalPriceArr.push(parseInt(priceFinder.price));
-
     html += `<div class="ordered">
           <div class="name-remove">
             <h3>${order}</h3>
@@ -117,7 +132,7 @@ document.addEventListener("submit", (e) => {
     <button class="special-button" type="submit">Complete order</button>
 
 </form>`;
-  }, 2000);
+  }, 3000);
 });
 
 modalOpener.addEventListener("click", () => {
@@ -127,3 +142,23 @@ modalOpener.addEventListener("click", () => {
 headerClick.addEventListener("click", () => {
   window.location.href = "#menu";
 });
+
+themeChange.addEventListener("click", () => {
+  renderBackground();
+});
+
+function renderBackground() {
+  if (isLightMode) {
+    document.body.style.background =
+      "radial-gradient(circle, #16db99, #00c3b6, #00a7c5, #0089c1, #0069a9)";
+    themeChange.classList.remove("fa-moon");
+    themeChange.classList.add("fa-sun");
+    localStorage.setItem("isLightMode", "enabled");
+  } else {
+    document.body.style.background = "#1c1c1c";
+    localStorage.setItem("isLightMode", "disabled");
+    themeChange.classList.remove("fa-sun");
+    themeChange.classList.add("fa-moon");
+  }
+  isLightMode = !isLightMode;
+}
