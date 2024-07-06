bd691ec64231f505ee96d7733ff4b3f5862c0f57
DavidDerama <davidbrentderama@gmail.com>
2024-07-06 11:02:34 +0300

    most of the website done

--- Autosivusto Sql/includes/script.js
+++ Autosivusto Sql/includes/script.js
@@ -1,1 +1,93 @@
-const main = document.querySelector("main");
+const autotContainer = document.getElementById("autot-koti");
+const hakuTulos = document.getElementById("hakutulos");
+const hakuForm = document.getElementById("auto-haku");
+const hakuLocal = JSON.parse(localStorage.getItem("hakutulos"));
+const modal = document.querySelector("dialog");
+const lisaaBtn = document.getElementById("lisaa-btn");
+const lisaaForm = document.getElementById("lisaa-form");
+
+document.addEventListener("DOMContentLoaded", () => {
+  modal.showModal();
+
+  fetch("/api/autot", {
+    method: "GET",
+    headers: { "Content-Type": "applciation/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const html = data
+        .map((el) => {
+          return `<div class="auto">
+        <div class="auto-img">
+          <img
+            src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div class="auto-desc">
+          <p>${el.vuosimalli}</p>
+          <h4>${el.merkki}, ${el.malli}</h4>
+          <p>${el.omistaja}</p>
+        </div>
+      </div>`;
+        })
+        .join("");
+      autotContainer.innerHTML = html;
+    });
+});
+
+hakuForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(hakuForm);
+  const hakusana = formData.get("hakusana");
+  fetch("/hae", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      hakusana: hakusana,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const json = [...data];
+      localStorage.setItem("hakutulos", JSON.stringify(json));
+      window.location.href = "/hakutulos";
+    });
+});
+
+if (hakuTulos) {
+  const hakuHtml = hakuLocal
+    .map((el) => {
+      return `<div class="auto">
+    <div class="auto-img">
+      <img
+        src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </div>
+    <div class="auto-desc">
+      <p>${el.vuosimalli}</p>
+      <h4>${el.merkki}, ${el.malli}</h4>
+      <p>${el.omistaja}</p>
+    </div>
+  </div>`;
+    })
+    .join("");
+  hakuTulos.innerHTML = hakuHtml;
+}
+
+lisaaBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+lisaaForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(lisaaForm);
+  const merkki = formData.get("merkki");
+  const malli = formData.get("malli");
+  const vuosimalli = formData.get("vuosimalli");
+  const omistaja = formData.get("omistaja");
+
+  console.log(merkki);
+});
--- Autosivusto Sql/palvelin.js
+++ Autosivusto Sql/palvelin.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql2");
 
 const { port, host } = require("./config.json");
+const dbconfig = require("./dbconfig.json");
 
 const app = express();
 
@@ -16,6 +18,58 @@ app.get("/", (req, res) => {
   res.render("main");
 });
 
+app.get("/hakutulos", (req, res) => {
+  res.render("hakutulos");
+});
+
+app.get("/api/autot", async (req, res) => {
+  try {
+    const autot = await haeAutot();
+    res.json(autot);
+  } catch (err) {
+    res.status(500).json({ err: "Autot ei löydetty" });
+  }
+});
+
+app.post("/hae", async (req, res) => {
+  const haettava = req.body.hakusana;
+
+  try {
+    const autot = await haeAutot();
+    const autoJson = autot.filter((auto) => {
+      if (
+        auto.malli.includes(haettava) ||
+        auto.merkki.includes(haettava) ||
+        auto.vuosimalli === parseInt(haettava) ||
+        auto.omistaja.includes(haettava)
+      ) {
+        return auto;
+      }
+    });
+    res.json(autoJson);
+  } catch (err) {
+    res.status(500).json({ err: "Autot ei löydetty" });
+  }
+});
+
+function haeAutot() {
+  const connection = mysql.createConnection(dbconfig);
+  connection.connect();
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM auto", (err, rivit, kentat) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rivit);
+    });
+    connection.end();
+  });
+}
 app.listen(port, host, () => {
   console.log("Palvelin kuuntelee");
 });
+
+function lisaaAuto() {
+  const connection = mysql.createConnection(dbconfig);
+  connection.connect();
+}
