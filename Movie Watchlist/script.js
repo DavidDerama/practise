11f43eadee4daaa62c6759dfd9f694327c35ec07
DavidDerama <davidbrentderama@gmail.com>
2024-07-09 03:59:10 +0300

    Trailer feature

--- Movie Watchlist/script.js
+++ Movie Watchlist/script.js
@@ -11,38 +11,9 @@ if (watchlist) {
 }
 
 if (watchlistEl) {
-  form.style.display = "none";
   if (watchlist) {
     getWatchlist();
   }
-}
-
-function getWatchlist() {
-  watchlistEl.innerHTML = "";
-  watchlist.forEach((movie, index) => {
-    fetch(`https://www.omdbapi.com/?i=${movie}&apikey=70717e7f&type=movie`)
-      .then((res) => res.json())
-      .then((data) => {
-        watchlistEl.innerHTML += `<div class="movie">
-              <div class="movie-image">
-                  <img src="${data.Poster}" alt="">
-              </div>
-              <div class="movie-info">
-                  <div class="title-rating"><h3>${data.Title}</h3><p>⭐${data.imdbRating}</p></div>
-                  <ul>
-                      <li><p>${data.Year}</p></li>
-                      <li><p>${data.Runtime}</p></li>
-                      <li><p>${data.Genre}</p></li>
-                      <li class="watchlist-btn">
-                          <button class="add-watch" data-remove="${index}"><i class="fa-solid fa-minus" data-remove"${index}"></i></button>
-                          <p>Remove</p>
-                      </li>
-                  </ul>
-                  <div class="desc"><p>${data.Plot}</p></div>
-              </div>
-          </div> `;
-      });
-  });
 }
 
 if (homeMainEl) {
@@ -58,36 +29,6 @@ if (homeMainEl) {
         } else {
           getMovieDesc(data.Search.slice(0, 5));
         }
-      });
-  });
-}
-
-function getMovieDesc(arr) {
-  homeMainEl.innerHTML = "";
-  arr.forEach((item) => {
-    fetch(
-      `https://www.omdbapi.com/?i=${item.imdbID}&apikey=70717e7f&type=movie`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        homeMainEl.innerHTML += `<div class="movie">
-            <div class="movie-image">
-                <img src="${data.Poster}" alt="">
-            </div>
-            <div class="movie-info">
-                <div class="title-rating"><h3>${data.Title}</h3><p>⭐${data.imdbRating}</p></div>
-                <ul>
-                    <li><p>${data.Year}</p></li>
-                    <li><p>${data.Runtime}</p></li>
-                    <li><p>${data.Genre}</p></li>
-                    <li class="watchlist-btn">
-                        <button class="add-watch" data-movie-id="${data.imdbID}"><i class="fa-solid fa-plus" data-movie-id="${data.imdbID}"></i></button>
-                        <p>Watchlist</p>
-                    </li>
-                </ul>
-                <div class="desc"><p>${data.Plot}</p></div>
-            </div>
-        </div> `;
       });
   });
 }
@@ -112,9 +53,21 @@ document.addEventListener("click", (e) => {
   }
   if (e.target.dataset.remove) {
     const index = e.target.dataset.remove;
-    watchlist.splice(index, 1);
-    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    const newArr = watchlist.splice(index, 1);
+    localStorage.setItem("watchlist", JSON.stringify(newArr));
     getWatchlist();
+    renderNotif("Movie removed from watchlist", "fa-check");
+    if (!watchlist.length) {
+      watchlistEl.innerHTML = `<div class="welcome">
+            <h2><i class="fa-solid fa-film"></i></h2>
+            <h2>Add a movie to your watchlist</h2>
+        </div>`;
+    }
+  }
+
+  if (e.target.dataset.trailer) {
+    const movieId = e.target.dataset.trailer;
+    renderTrailer(movieId);
   }
 });
 
@@ -124,4 +77,90 @@ function renderNotif(message, status) {
     <i class="fa-solid ${status}"></i>
     <p class="label">${message}</p>
   </div>`;
+}
+
+function getMovieDesc(arr) {
+  homeMainEl.innerHTML = "";
+  arr.forEach((item) => {
+    fetch(
+      `https://www.omdbapi.com/?i=${item.imdbID}&apikey=70717e7f&type=movie`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        homeMainEl.innerHTML += `<div class="movie main-movie">
+              <div class="movie-image" data-trailer="${data.imdbID}">
+                  <img src="${data.Poster}" alt="" data-trailer="${data.imdbID}">
+              </div>
+              <div class="movie-info">
+                  <div class="title-rating" data-trailer="${data.imdbID}"><h3 data-trailer="${data.imdbID}">${data.Title}</h3><p>⭐${data.imdbRating}</p></div>
+                  <ul>
+                      <li><p>${data.Year}</p></li>
+                      <li><p>${data.Runtime}</p></li>
+                      <li><p>${data.Genre}</p></li>
+                      <li class="watchlist-btn">
+                          <button class="add-watch" data-movie-id="${data.imdbID}"><i class="fa-solid fa-plus" data-movie-id="${data.imdbID}"></i></button>
+                          <p>Watchlist</p>
+                      </li>
+                  </ul>
+                  <div class="desc"><p>${data.Plot}</p></div>
+              </div>
+          </div> `;
+      });
+  });
+}
+
+function getWatchlist() {
+  watchlistEl.innerHTML = "";
+  watchlist.forEach((movie, index) => {
+    fetch(`https://www.omdbapi.com/?i=${movie}&apikey=70717e7f&type=movie`)
+      .then((res) => res.json())
+      .then((data) => {
+        watchlistEl.innerHTML += `<div class="movie">
+                  <div class="movie-image">
+                      <img src="${data.Poster}" alt="">
+                  </div>
+                  <div class="movie-info">
+                      <div class="title-rating"><h3>${data.Title}</h3><p>⭐${data.imdbRating}</p></div>
+                      <ul>
+                          <li><p>${data.Year}</p></li>
+                          <li><p>${data.Runtime}</p></li>
+                          <li><p>${data.Genre}</p></li>
+                      </ul>
+                      <div class="desc">
+                      <p>${data.Plot}</p><button class="remove-watch" data-remove="${index}">Remove</button>
+                      </div>
+                      
+                  </div>
+              </div> `;
+      });
+  });
+}
+
+function renderTrailer(movieId) {
+  console.log(movieId);
+  fetch(
+    `https://api.themoviedb.org/3/find/${movieId}?external_source=imdb_id&api_key=3cbb651406e6c485b5275a6f20607d59`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const tmdbId = data.movie_results[0].id;
+      fetch(
+        `https://api.themoviedb.org/3/movie/${tmdbId}/videos?language=en-US&api_key=3cbb651406e6c485b5275a6f20607d59`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const results = data.results;
+          const filteredResults = results.filter((movie) => {
+            return movie.official && movie.type.includes("Trailer");
+          });
+          const trailerId = filteredResults[0].key;
+          const trailerLink = `https://www.youtube.com/embed/${trailerId}?si=-l0VOGSng81FHOP8`;
+          const trailerFrame = document.querySelector("iframe");
+
+          trailerFrame.src = trailerLink;
+          document.querySelector("header").style.filter = "blur(10px)";
+          document.querySelector("main").style.filter = "blur(10px)";
+          trailerFrame.style.display = "block";
+        });
+    });
 }
