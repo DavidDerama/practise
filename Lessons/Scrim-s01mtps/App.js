69d579b4d4ba549847c3a5a75da4fa24ff02375f
DavidDerama <davidbrentderama@gmail.com>
2024-07-26 15:37:52 +0300

    dasdadasd

---
+++ Lessons/Scrim-s01mtps/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Die from "./Die"
+
+/**
+ * Challenge:
+ * 
+ * Write a function (allNewDice) that returns an array 
+ * of 10 random numbers between 1-6 inclusive.
+ * 
+ * Log the array of numbers to the console for now
+ */
+
+export default function App() {
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    console.log(allNewDice())
+    
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
+}
