60145a401038c3659e89781f056a00fb0527a7ab
DavidDerama <davidbrentderama@gmail.com>
2024-08-18 08:02:53 +0300

    2

---
+++ Lessons/Scrim-s0uo3jt4g3/components/ImageContainer.js
@@ -0,0 +1,15 @@
+export default function ImageContainer(){
+    return (
+        <div className="image-container">
+                            <div className="image-wrapper">
+                                <img className="hot-dog" src="images/hotdogs.jpg"/>
+                            </div>
+                            <div className="image-wrapper">
+                                <img src="images/sandwich.jpg"/>
+                            </div> 
+                            <div className="image-wrapper">
+                                <img src="images/hamburger.jpg"/>
+                            </div>                                  
+         </div>
+    )
+}
