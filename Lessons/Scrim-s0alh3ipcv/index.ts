394522133f0cd774005fc7a25b28d49d69c410b0
DavidDerama <davidbrentderama@gmail.com>
2024-09-26 17:09:23 +0300

    dassad

---
+++ Lessons/Scrim-s0alh3ipcv/index.ts
@@ -0,0 +1,28 @@
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+
+
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
