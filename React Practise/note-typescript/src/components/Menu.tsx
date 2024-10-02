0f72d5e5621820bf088a9ada30b9b2975bedd5c9
DavidDerama <davidbrentderama@gmail.com>
2024-10-02 20:15:36 +0300

    dsadasd

--- React Practise/note-typescript/src/components/Menu.tsx
+++ React Practise/note-typescript/src/components/Menu.tsx
@@ -1,6 +1,6 @@
 export const Menu = () => {
   return (
-    <form>
+    <form className="menu">
       <input type="text" />
       <div className="menu-buttons">
         <button type="submit">Send</button>
