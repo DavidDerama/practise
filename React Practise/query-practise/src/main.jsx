48c4286b27fd321f96fe0322093479aeceb9fb9c
DavidDerama <davidbrentderama@gmail.com>
2024-10-10 03:29:26 +0300

    garbage collection exmaple

--- React Practise/query-practise/src/main.jsx
+++ React Practise/query-practise/src/main.jsx
@@ -3,7 +3,9 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 3000, gcTime: 10 * (60 * 1000) } },
+});
 const root = createRoot(document.getElementById("root"));
 
 root.render(
