2f368c8269449a4f3cd3d64780af388d0b3c191e
DavidDerama <davidbrentderama@gmail.com>
2024-10-14 20:00:09 +0300

    form changes

--- React Practise/autosivusto/client/src/pages/AddCar.tsx
+++ React Practise/autosivusto/client/src/pages/AddCar.tsx
@@ -3,10 +3,24 @@ export const AddCar = () => {
     <main>
       <div className="content">
         <h1>Add a car</h1>
-        <form>
+        <form className="lisaa-form">
           <div className="form-input">
-            <label htmlFor=""></label>
+            <label htmlFor="merkki">Merkki:</label>
+            <input type="text" name="merkki" />
           </div>
+          <div className="form-input">
+            <label htmlFor="malli">Malli:</label>
+            <input type="text" name="malli" />
+          </div>
+          <div className="form-input">
+            <label htmlFor="vuosimalli">Vuosimalli:</label>
+            <input type="text" name="vuosimalli" />
+          </div>
+          <div className="form-input">
+            <label htmlFor="omistaja">Omistaja:</label>
+            <input type="text" name="omistaja" />
+          </div>
+          <button>Add</button>
         </form>
       </div>
     </main>
