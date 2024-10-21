fa2e0282d5b7e94eb69ff596bc148dc1091a53ea
DavidDerama <davidbrentderama@gmail.com>
2024-10-21 17:52:07 +0300

    remove button completely working

--- React Practise/autosivusto/client/src/components/AutotBoxi.tsx
+++ React Practise/autosivusto/client/src/components/AutotBoxi.tsx
@@ -1,12 +1,27 @@
 import { Auto } from "../shared/types";
 import { FaCarOn } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa6";
+import { useMutation, useQueryClient } from "react-query";
 
 type AutotBoxiProps = {
   autot: Auto[];
 };
 
 export const AutotBoxi = ({ autot }: AutotBoxiProps) => {
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: (autoId) =>
+      fetch("http://localhost:3000/api/poista", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+        body: JSON.stringify(autoId),
+      }).then((res) => res.json()),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: "autot" });
+    },
+  });
+
   const displayedAutot = autot?.map((auto) => {
     return (
       <div className="auto-boxi">
@@ -18,7 +33,10 @@ export const AutotBoxi = ({ autot }: AutotBoxiProps) => {
         </h3>
         <p>{auto.omistaja}</p>
         <p className="label">{auto.vuosimalli}</p>
-        <button className="remove-car-btn">
+        <button
+          className="remove-car-btn"
+          onClick={() => mutate({ id: auto.id })}
+        >
           <FaTrash />
         </button>
       </div>
