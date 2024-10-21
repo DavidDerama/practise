e7d7c984ce3d62176db9d0ba4d159ec488be9729
DavidDerama <davidbrentderama@gmail.com>
2024-10-21 18:13:31 +0300

    site fully done

--- React Practise/autosivusto/client/src/components/AutotBoxi.tsx
+++ React Practise/autosivusto/client/src/components/AutotBoxi.tsx
@@ -2,15 +2,18 @@ import { Auto } from "../shared/types";
 import { FaCarOn } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa6";
 import { useMutation, useQueryClient } from "react-query";
+import { useLocation } from "react-router-dom";
 
 type AutotBoxiProps = {
   autot: Auto[];
 };
 
 export const AutotBoxi = ({ autot }: AutotBoxiProps) => {
+  const location = useLocation();
+
   const queryClient = useQueryClient();
   const { mutate } = useMutation({
-    mutationFn: (autoId) =>
+    mutationFn: (autoId: { id: number }) =>
       fetch("http://localhost:3000/api/poista", {
         method: "DELETE",
         headers: { "Content-Type": "application/json" },
@@ -21,6 +24,10 @@ export const AutotBoxi = ({ autot }: AutotBoxiProps) => {
       queryClient.invalidateQueries({ queryKey: "autot" });
     },
   });
+
+  function removeCar(autoId: number) {
+    mutate({ id: autoId });
+  }
 
   const displayedAutot = autot?.map((auto) => {
     return (
@@ -33,12 +40,11 @@ export const AutotBoxi = ({ autot }: AutotBoxiProps) => {
         </h3>
         <p>{auto.omistaja}</p>
         <p className="label">{auto.vuosimalli}</p>
-        <button
-          className="remove-car-btn"
-          onClick={() => mutate({ id: auto.id })}
-        >
-          <FaTrash />
-        </button>
+        {location?.pathname !== "/haku" && (
+          <button className="remove-car-btn" onClick={() => removeCar(auto.id)}>
+            <FaTrash />
+          </button>
+        )}
       </div>
     );
   });
