382dc0ece9c661434fd1688fb4696fe8419970ad
DavidDerama <davidbrentderama@gmail.com>
2024-11-11 13:28:17 +0200

    python

---
+++ Lessons/Scrim-s01uap1/index.py
@@ -0,0 +1,8 @@
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+print(friends)
+print(friends_tuple)
+print(friends_set)
+
