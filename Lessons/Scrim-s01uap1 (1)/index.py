3bf033f2b6fb9ed12ef434b8c53941af0d643b1c
DavidDerama <davidbrentderama@gmail.com>
2024-11-11 13:28:11 +0200

    python

---
+++ Lessons/Scrim-s01uap1 (1)/index.py
@@ -0,0 +1,9 @@
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+
+print(friends_set.difference(my_friends_set))
+
+
