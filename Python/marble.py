36913d2a1ae4dcdd540260ea052966b2e76c923f
DavidDerama <davidbrentderama@gmail.com>
2024-11-14 17:05:26 +0200

    python

--- Python/marble.py
+++ Python/marble.py
@@ -1,19 +1,34 @@
 print('Project - Trading game simulation / pseudo code')
 import random 
 
-gold_pieces = 1000
+start_gold = 1000
+gold_pieces = start_gold
+bag_inventory = ("green","green","green","green","black","green","red","red","red","white")
+rounds = 3
+marble = "none"
 
-
-bag_inventory = ("green","green","green","green","green","green","red","red","red","red")
-
-while gold_pieces > 0:
-    print(f"You currently have {gold_pieces} pieces of gold.")
+for i in range(1, rounds + 1):
+    print(f"Round {i}")
+    print(f"Last draw you got a {marble} marble. You currently have {gold_pieces} pieces of gold.")
     bet_amount = int(input(f"How much are you betting?"))
-    random_marble = random.choices(bag_inventory)[0]
-    if(random_marble == "green"):
-        gold_pieces += bet_amount
-        print(f"You got a {random_marble} marble and won {bet_amount} pieces of gold")
-    else: 
-        gold_pieces -= bet_amount
-        print(f"You got a {random_marble} marble and lost {bet_amount} pieces of gold")
+    marble = random.choices(bag_inventory)[0]
+    if(marble == "green"):
+            gold_pieces += bet_amount
+            print(f"You got a {marble} marble and won {bet_amount} pieces of gold")
+    elif(marble == "black"):
+            gold_pieces += bet_amount * 10
+            print(f"You got a {marble} marble and won {bet_amount} pieces of gold")
+    elif(marble == "white"):
+            gold_pieces -= bet_amount * 5
+            print(f"You got a {marble} marble and lost {bet_amount} pieces of gold")
+    elif(marble == "red"): 
+            gold_pieces -= bet_amount
+            print(f"You got a {marble} marble and lost {bet_amount} pieces of gold")
+
+    if gold_pieces < 0.5 * start_gold:
+        print("You lost the game")
+        break
+
+print("Starting/Ending Gold:", start_gold, "/", gold_pieces)
+print("Gain/Loss", ((gold_pieces / start_gold) * 100), "%" )
     
