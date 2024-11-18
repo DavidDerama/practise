0a958255d48f8fc54996b4bcdc4ccb58d0d0863a
DavidDerama <davidbrentderama@gmail.com>
2024-11-18 22:16:10 +0200

    python

---
+++ Lessons/Scrim-s01uapj/index.py
@@ -0,0 +1,9 @@
+movie = {
+    'title' : 'Life of Brian',
+    'year' : 1979,
+    'cast' : ['John','Eric','Michael','George','Terry']
+}
+for key, value in movie.items():
+    print(key, value)
+
+
