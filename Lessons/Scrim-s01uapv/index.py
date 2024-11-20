a89e552b1289e977b6e44268b209fe3fb13935a4
DavidDerama <davidbrentderama@gmail.com>
2024-11-20 22:01:55 +0200

    python

---
+++ Lessons/Scrim-s01uapv/index.py
@@ -0,0 +1,22 @@
+def enigma_light():
+# create keys string
+    keys = 'abcdefghijklmnopqrstuvwxyz !'
+# autogenerate the values string by offsetting original string
+    values = keys[-1] + keys[0:-1]
+    print(keys)
+    print(values)
+# create two dictionaries
+    dict_e = dict(zip(keys,values))
+    dict_d = dict(zip(values,keys)) 
+# OR create 1 and then flip 
+    dict_e = dict(zip(keys,values))
+    dict_d = {value:key for key, value in dict_e.items()}
+#user input 'the message' and mode
+    msg = input('Enter your secret message quietly: ')
+    mode = input('Crypto mode: encode (e) OR decode (d): ')
+# run encode or decode
+    
+# return result
+# clean and beautify the code 
+
+enigma_light()
